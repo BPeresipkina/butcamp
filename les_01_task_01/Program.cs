dc444ead6b3f1d3b62ed23b260f87092f16e4bc9
Barbara Peres <barbarabelyaeva13@gmail.com>
2023-02-20 12:03:52 +0300

    les_01

--- les_01_task_01/Program.cs
+++ les_01_task_01/Program.cs
@@ -1,1 +1,47 @@
-﻿
+﻿using System.Diagnostics;
+
+// Дан массив, выдать MAX сумму последовательности n чисел
+
+// 1 5 4 1 3 4 7 4 1 2
+// m = 3
+// MAX -> 15
+
+int size = 1_000_000;
+
+int m = 30_000;
+
+int[] array = Enumerable.Range(1, size)
+                        .Select(item => Random.Shared.Next(10)) //значек следует
+                        .ToArray();
+
+// Console.WriteLine($"[{string.Join(", ", array)}]");
+
+Stopwatch sw = new();
+sw.Start();
+
+// int max = 0;
+
+// for (int i = 0; i < array.Length - m; i++)
+// {
+//     int t = 0;
+//     for (int j = i; j < i + m; j++) t += array[j];
+//     if (t > max) max = t;
+// }
+
+// sw.Stop();
+// Console.WriteLine($"time = {sw.ElapsedMilliseconds}");
+// Console.WriteLine(max);
+
+int max1 = 0;
+
+for (int j = 0; j < m; j++) max1 += array[j];
+int t1 = max1;
+for (int i = 1; i < array.Length - m; i++)
+{
+    t1 = t1 - array[i - 1] + array[i + m - 1];
+    if (t1 > max1) max1 = t1;
+}
+
+sw.Stop();
+Console.WriteLine($"time = {sw.ElapsedMilliseconds}");
+Console.WriteLine(max1);
