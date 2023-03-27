41831e99027b1573c4f92c0f59321ba480ec4669
Barbara Peres <barbarabelyaeva13@gmail.com>
2023-03-27 20:01:38 +0300

    Полноценный проект сортировки и оформления проекта готов

--- les_04_task_01/Infrastructure.cs
+++ les_04_task_01/Infrastructure.cs
@@ -3,15 +3,16 @@ using static System.String; // чтобы не писать String.
 
 public static class Infrastructure
 {
-    public static int[] CreatArray(int size, int min = 0, int max = 10)
+    public static int[] CreatArray(this int size, int min = 0, int max = 10)
     {
         return Enumerable.Range(1, size)
                 .Select(item => Random.Shared.Next(min, max))
                 .ToArray();
     }
 
-    public static void Show(int[] array)
+    public static int[] Show(this int[] array)
     {
         WriteLine($"[{Join(',', array)}]");
+        return array;
     }
 }
--- les_04_task_01/Program.cs
+++ les_04_task_01/Program.cs
@@ -1,7 +1,12 @@
 ﻿using static Sorting;
 using static Infrastructure;
 
-int[] array = CreatArray(10);
-Show(array);
-SortSelection(array);
-Show(array);
+// int[] array = CreatArray(10);
+// Show(array);
+// SortSelection(array);
+// Show(array);
+
+10.CreatArray(min: 10, max: 30)
+  .Show()
+  .SortSelection()
+  .Show();
